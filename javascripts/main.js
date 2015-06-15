8dfd42410d6bc66382a09c1f38592e375017d5e2
Travis Hooper <travis.d.hooper@gmail.com>
2015-06-15 01:27:27 -0500

    master: Adds clickable modal to post pictures. Fixes alignment issue with post slideshow. Adds Blazer to current projects

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
     $('.pgwSlideshow').pgwSlideshow();
+
+    $('.ps-current ul li img').on('click', function() {
+        var src = $(this).attr('src').replace(/\/images\//, '').replace(/\..*/, '');
+        $.pgwModal({
+            target: '#' + src + '-modal',
+            titleBar: false,
+            maxWidth: 2000
+        });
+    });
+
 });
