bbb218c12dd7a74fee3234bf91ab14df8a79404c
Travis Hooper <travis.d.hooper@gmail.com>
2015-06-16 18:34:58 -0500

    master: completes moving images into their own dirs

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     $('.pgwSlideshow').pgwSlideshow();
 
     $('.ps-current ul li img').on('click', function() {
-        var src = $(this).attr('src').replace(/\/images\//, '').replace(/\..*/, '');
+        var src = $(this).attr('src').replace(/\/images\/.*\//, '').replace(/\..*/, '');
+        console.log(src)
         $.pgwModal({
             target: '#' + src + '-modal',
             titleBar: false,
