2ea7d1dd44180b3be607d0ecaecd0683474445f2
Travis Hooper <travis.d.hooper@gmail.com>
2015-06-16 22:13:53 -0500

    master: Adds responsive modal image

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,47 @@
 $(document).ready(function() {
     $('.pgwSlideshow').pgwSlideshow();
 
+    var $modalImage
+
     $('.ps-current ul li img').on('click', function() {
-        var src = $(this).attr('src').replace(/\/images\/.*\//, '').replace(/\..*/, '');
-        console.log(src)
+        var $image = $(this)
+        var src = $image.attr('src').replace(/\/images\/.*\//, '').replace(/\..*/, '');
+        var id = '#' + src + '-modal'
         $.pgwModal({
-            target: '#' + src + '-modal',
+            target: id,
             titleBar: false,
             maxWidth: 2000
         });
+
+        $modalImage = $('.pm-container .pm-body .pm-content img')
+
+        function adjustImageSize() {
+            var windowHeight = $(window).height(),
+                windowWidth = $(window).width(),
+                windowRatio = windowWidth / windowHeight,
+                imgHeight = $image.height(),
+                imgWidth = $image.width(),
+                imgRatio = imgWidth / imgHeight
+            if (windowRatio >= imgRatio) {
+                var imageContainerWidth = (windowHeight * 0.95 * imgRatio) + 20
+                $('.pm-body').css('width', imageContainerWidth)
+                $modalImage.addClass('over-square')
+            } else {
+                $('.pm-body').css('width', '95%')
+                $modalImage.removeClass('over-square')
+            }
+        }
+
+        adjustImageSize()
+
+        $(window).resize(function() {
+            adjustImageSize()
+        });
+
     });
+
+
+
+
 
 });
